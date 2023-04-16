61714afdcf366a755080488ade9c50f76a9658c1
DeflatedPickle <deflatedpickle@gmail.com>
2023-04-16 01:22:24 +0100

    Added particles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,14 +7,27 @@ public class Player : MonoBehaviour
 {
     private Rigidbody rigidbodyComponent;
 
+    [Header("Component")]
+    [SerializeField] private SpriteRenderer sprite;
+
+    [SerializeField] private ParticleSystem jumpParticles;
+    [SerializeField] private ParticleSystem thrustParticles;
+
     private Vector2 move;
 
     private bool isGrounded;
     private bool canDoubleJump;
 
+    [Header("Movement")]
     [SerializeField] private float moveSpeed = 8f;
     [SerializeField] private float jumpPower = 10f;
     [SerializeField] private float doubleJumpPower = 5f;
+
+    [Header("Animation")]
+    [SerializeField] private Vector2 jumpSqueeze;
+    [SerializeField] private float jumpSqueezeSpeed = 0.1f;
+    [SerializeField] private Vector2 thrustSqueeze;
+    [SerializeField] private float thrustSqueezeSpeed = 0.2f;
 
     private void Start()
     {
@@ -40,6 +53,9 @@ public class Player : MonoBehaviour
                 ForceMode.Impulse
             );
 
+            StartCoroutine(Squeeze(jumpSqueeze.x, jumpSqueeze.y, jumpSqueezeSpeed));
+            jumpParticles.Play();
+
             isGrounded = false;
             canDoubleJump = true;
         } else {
@@ -48,14 +64,45 @@ public class Player : MonoBehaviour
                 rigidbodyComponent.AddForce(
                     new Vector3(
                         0,
-                        jumpPower,
+                        doubleJumpPower,
                         0
                     ),
                     ForceMode.Impulse
                 );
 
+                StartCoroutine(Squeeze(thrustSqueeze.x, thrustSqueeze.y, thrustSqueezeSpeed));
+                thrustParticles.Play();
+
                 canDoubleJump = false;
             }
+        }
+    }
+
+    IEnumerator Squeeze(float x, float y, float seconds)
+    {
+        var originalSize = Vector3.one;
+        var newSize = new Vector3(x, y, originalSize.z);
+
+        var t = 0f;
+
+        while (t <= 1)
+        {
+            t += Time.deltaTime / seconds;
+
+            sprite.transform.localScale = Vector3.Lerp(originalSize, newSize, t);
+            
+            yield return null;
+        }
+
+        t = 0f;
+
+        while (t <= 1)
+        {
+            t += Time.deltaTime / seconds;
+
+            sprite.transform.localScale = Vector3.Lerp(newSize, originalSize, t);
+
+            yield return null;
         }
     }
 
